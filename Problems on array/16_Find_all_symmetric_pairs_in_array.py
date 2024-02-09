5cfe728135c58e4364321d5c6cef789e573a1e64
HItesh Ankodia <80949719+Hiteshankodia@users.noreply.github.com>
2024-02-09 22:55:42 +0530

    Update 16_Find_all_symmetric_pairs_in_array.py

--- Problems on array/16_Find_all_symmetric_pairs_in_array.py
+++ Problems on array/16_Find_all_symmetric_pairs_in_array.py
@@ -1,11 +1,23 @@
 #Input: (1,2),(2,1),(3,4),(4,5),(5,4)
 #Output: (2,1) (5,4)
 
-list1 = [[1,2],[2,1],[3,4],[4,5],[5,4]]
-for i in list1:
-    a = (i[1], i[0])
-    a =list(a)
+a = [(1,2),(2,1),(3,4),(4,5),(5,4)]
+sym_pairs = []
+for i in range(len(a)):
+    for j in range(len(a)):
+        if a[i] == a[j][::-1]:
+            sym_pairs.append(a[i])
+            
+result = []
+for i in sym_pairs:
+    if not i[::-1] in result:
+        result.append(i)
+    else:
+        continue
+print(result)    
+        
     
-    if a in list1:
-        print(i)
+
+            
+
     
